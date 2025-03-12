0f7e81152ab0acb134ac262ad6eed7dafdf5ae44
delia_recio <delia_recio@epam.com>
2025-03-12 13:49:08 +0100

    Adding Example test to integers module

--- Integers/adder_test.go
+++ Integers/adder_test.go
@@ -1,6 +1,9 @@
 package Integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -10,3 +13,14 @@ func TestAdder(t *testing.T) {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
 }
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+// The comment is needed. Examples without comments are
+//useful for demonstrating code that cannot run as unit tests
+//(like test that access the network)
+//while guaranteeing that the example at least compiles
